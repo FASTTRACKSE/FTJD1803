feca6d66a810f1475edd14603976223a6d765654
nguyenhuyvu <hvucool@gmail.com>
2018-11-09 20:01:16 +0700

    Update HuyVu/Session1/src/vonglap/BangCuuChuong.java

--- HuyVu/Session1/src/vonglap/BangCuuChuong.java
+++ HuyVu/Session1/src/vonglap/BangCuuChuong.java
@@ -1,15 +1,16 @@
 package vonglap;
 
-public class Bangcuuchuong {
+public class BangCuuChuong {
 
 	public static void main(String[] args) {
 
 		for(int i = 2 ; i<=10 ; i++) {
 			for(int j = 1 ; j <=10 ; j++) {
-				System.out.println( i+ "x" +j + "=" +i*j);
+				System.out.print(i + "x" + j + "=" + i*j + "\t" );
 			}
+			System.out.println("\n");
 		}
-		System.out.println("");
+		
 	}
 
 }
