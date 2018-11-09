af58381e11958280d688002f0d0d63f2ac65f7bc
hoquanghieu963 <44837911+hoquanghieu963@users.noreply.github.com>
2018-11-09 20:09:02 +0700

    new

--- Quang Hieu/HelloJava/src/TinhTienDien.java
+++ Quang Hieu/HelloJava/src/TinhTienDien.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class TinhTienDien {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	
 	int a;
+	int b;
+	Scanner sc = new Scanner(System.in);
+	a = Integer.parseInt(sc.nextLine());
+	if (a < 50)
+	{
+		System.out.println(a*1000);
+	}
+	if ( a > 50)
+	{
+		System.out.println(50*1000 + (a-50)*1200);
+	}
+}
 }
