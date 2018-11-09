7eb986a9e56dcc16e8ada650ef34e868c0026721
beerin99 <44837865+beerin99@users.noreply.github.com>
2018-11-09 20:07:54 +0700

    addBangCuuChuong

---
+++ Thuan/bangCuuChuong/src/bangCuuChuong/bCC.java
@@ -0,0 +1,16 @@
+package bangCuuChuong;
+
+public class bCC {
+			public static void main(String arg[]) {
+				for ( int i = 1 ; i <= 10 ; i ++){
+		            for ( int j = 2; j<= 9 ; j ++) {
+		            	
+		                System.out.print(j + " x " + i + " = " + i * j+ "\t" );
+		            }
+		           
+
+		            System.out.print("\n");
+			}
+			}
+}
+
