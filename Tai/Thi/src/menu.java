c5c07dd1e4090f676434c6ab5fe0101d014be478
Taiphan123 <tai_1651220060@dau.edu.vn>
2018-12-20 10:23:01 +0700

    add

--- Tai/Thi/src/menu.java
+++ Tai/Thi/src/menu.java
@@ -8,7 +8,7 @@ public class menu {
 
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
-		  int n2,n1;
+		  int n2 = 0,n1 = 0;
 
 			 ArrayList<DoiBong> db = new ArrayList<>();
 			System.out.println("1. Nhap danh sach huan luan vien va cau thu.\r\n" + "2. Hien danh sach cau thu\r\n"
@@ -40,63 +40,63 @@ public class menu {
 				}
 			
 				break;
-//			case 2: {
-//				for (DoiBong ps : db) {
-//					if (ps instanceof Player) {
-//						Player pl = (Player) ps;
-//						pl.xuat();
-//					}
-//				}
-//			}
-//				break;
-//			case 3:
-//				for (DoiBong ps : db) {
-//					if (ps instanceof Coach) {
-//						Coach ch = (Coach) ps;
-//						ch.xuat();
-//					}
-//				}
-//				break;
-//			case 4:{
-//				for (DoiBong ps : db) {
-//					ps.xuat();
-//				}
-//			}			
-//				break;
-//			case 5:
-//			{
-//				for (int i = 0; i < n2; i++) {
-//					Comparator<Player> comparator = new Comparator<Player>() {
-//
-//						@Override
-//						public int compare(Player o1, Player o2) {
-//
-//							if (o1.getHoTen().compareTo(o2.getHoTen()) > 0) {
-//								return 1;
-//							} else {
-//								return 0;
-//							}
-//						}
-//					};
-//				}
-//				System.out.println("da sap xep!");
-//			}break;
-//			case 6:{
-//			float tong1=0;
-//			for (int i = 0; i < n1; i++) {
-//				DoiBong pl = new Player();
-//				tong1+=pl.tinhThunhap();
-//			}
-//			float tong2 = 0;
-//			for (int i = 0; i < n2; i++) {
-//				DoiBong coach = new Coach();
-//				tong2+=coach.tinhThunhap();
-//			}
-//			System.out.println("Tong luong phai tra la:"+(tong1+tong2));
-//			}break;
-//			case 7:{
-//				
-//			}break;
+			case 2: {
+				for (DoiBong ps : db) {
+					if (ps instanceof Player) {
+						Player pl = (Player) ps;
+						pl.xuat();
+					}
+				}
+			}
+				break;
+			case 3:
+				for (DoiBong ps : db) {
+					if (ps instanceof Coach) {
+						Coach ch = (Coach) ps;
+						ch.xuat();
+					}
+				}
+				break;
+			case 4:{
+				for (DoiBong ps : db) {
+					ps.xuat();
+				}
+			}			
+				break;
+			case 5:
+			{
+				for (int i = 0; i < n2; i++) {
+					Comparator<Player> comparator = new Comparator<Player>() {
+
+						@Override
+						public int compare(Player o1, Player o2) {
+
+							if (o1.getHoTen().compareTo(o2.getHoTen()) > 0) {
+								return 1;
+							} else {
+								return 0;
+							}
+						}
+					};
+				}
+				System.out.println("da sap xep!");
+			}break;
+			case 6:{
+			float tong1=0;
+			for (int i = 0; i < n1; i++) {
+				DoiBong pl = new Player();
+				tong1+=pl.tinhThunhap();
+			}
+			float tong2 = 0;
+			for (int i = 0; i < n2; i++) {
+				DoiBong coach = new Coach();
+				tong2+=coach.tinhThunhap();
+			}
+			System.out.println("Tong luong phai tra la:"+(tong1+tong2));
+			}break;
+			case 7:{
+				
+			}break;
 			}
 		}
 	}
