bbe7a3affee185b71c32514a2d7f4febe8383978
nguyenhuyvu <hvucool@gmail.com>
2019-01-02 21:04:15 +0700

    update new

---
+++ HuyVu/QuanLySinhVienTree/src/fasttrackse/vn/edu/model/LopHoc.java
@@ -0,0 +1,35 @@
+package fasttrackse.vn.edu.model;
+
+public class LopHoc {
+	private int maLop;
+	private String tenLop;
+	private int siSo;
+	private int maKhoa;
+	
+	public int getMaLop() {
+		return maLop;
+	}
+	public void setMaLop(int maLop) {
+		this.maLop = maLop;
+	}
+	public String getTenLop() {
+		return tenLop;
+	}
+	public void setTenLop(String tenLop) {
+		this.tenLop = tenLop;
+	}
+	public int getSiSo() {
+		return siSo;
+	}
+	public void setSiSo(int siSo) {
+		this.siSo = siSo;
+	}
+	public int getMaKhoa() {
+		return maKhoa;
+	}
+	public void setMaKhoa(int maKhoa) {
+		this.maKhoa = maKhoa;
+	}
+	
+	
+}
---
+++ HuyVu/QuanLySinhVienTree/src/fasttrackse/vn/edu/test/TestMainUI.java
@@ -0,0 +1,14 @@
+package fasttrackse.vn.edu.test;
+
+import fasttrackse.vn.edu.ui.MainUI;
+
+public class TestMainUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainUI mainUI = new MainUI("Quan ly sinh vien dang cay");
+		mainUI.showWindow();
+
+	}
+
+}
